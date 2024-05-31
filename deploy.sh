83bc1d066c758ac7aac0d8086d413604f5854d2a
Attila Kerekes <k.attila@gmail.com>
2024-05-31 21:17:20 +0200

    Add docker file Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit script on any error
+set -e
+
+# Source the .env file
+if [ -f .env ]; then
+  source .env
+else
+  echo ".env file not found!"
+  exit 1
+fi
+
+echo "Building the App..."
+npm run build
+
+echo "Creating Docker image..."
+docker build --platform=linux/amd64 -t $GAME_REGISTRY:latest .
+
+echo "Pushing Docker image to registry..."
+docker push $GAME_REGISTRY:latest
+
+echo "Container pushed!"
